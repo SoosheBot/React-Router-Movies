bf3b9dbaee8c5aa16219e2701c4b54e5777f3885
sooshe <sooshe@sooshebot.com>
2019-09-17 16:47:51 -0500

    turned the cards into links in movielist.js, modified line13 of movie.js

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
 import axios from 'axios';
+
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -30,7 +32,9 @@ const MovieList = props => {
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
+    
     <div className="movie-card">
+      <NavLink to={`/movies.${movie.id}`}>
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
@@ -45,6 +49,7 @@ function MovieDetails({ movie }) {
           {star}
         </div>
       ))}
+      </NavLink>
     </div>
   );
 }
